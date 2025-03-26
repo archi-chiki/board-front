6d27e8bbf3369e728f4d60f6d14728741ece74f8
김창희 <galahad0001@ucware.net>
2025-03-27 01:07:29 +0900

    ADD: 사용자 인증..? 기능 추가

--- src/components/login/LoginForm.tsx
+++ src/components/login/LoginForm.tsx
@@ -1,6 +1,10 @@
+import { useForm } from "react-hook-form";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import styled from "@emotion/styled";
+import formClient from "../../api/form-axios";
 
-const Container = styled.form`
+const Container = styled("form")`
   display: flex;
   flex-direction: column;
   gap: 12px;
@@ -31,12 +35,49 @@ const Button = styled.button`
   }
 `;
 
+interface LoginFormProps {
+  id: string;
+  password: string;
+}
+
 export default function LoginForm() {
+  const {
+    register,
+    handleSubmit,
+    setFocus,
+    reset,
+    formState: { errors },
+  } = useForm<LoginFormProps>();
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    setFocus("id");
+  }, [setFocus]);
+
+  const onSubmit = async (data: LoginFormProps) => {
+    try {
+      const response = await formClient.post("auth", data);
+      console.log("서버 응답:", response);
+
+      if (response.status === 200 && response.data.message === "success") {
+        navigate("/board");
+      }
+    } catch (error) {
+      console.error("에러 발생:", error);
+    }
+  };
+
   return (
-    <Container>
-      <h2>Haaaaalo?</h2>
-      <Input id="email" placeholder="Email" />
-      <Input id="password" placeholder="Password" />
+    <Container onSubmit={handleSubmit(onSubmit)}>
+      <h2>로그인 해주세요 제발</h2>
+      <Input id="id" placeholder="ID" {...register("id", { required: true })} />
+      <Input
+        id="password"
+        placeholder="Password"
+        type="password"
+        {...register("password", { required: true })}
+      />
       <Button type="submit">로그인</Button>
     </Container>
   );
